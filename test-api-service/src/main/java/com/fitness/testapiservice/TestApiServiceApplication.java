4829fef4fc345a5f28d130e3f9a9da7ca5a39b1a
Bence <borsi92@gmail.com>
2021-02-02 21:11:09 +0100

    added H2 and loaded test data into it

--- test-api-service/src/main/java/com/fitness/testapiservice/TestApiServiceApplication.java
+++ test-api-service/src/main/java/com/fitness/testapiservice/TestApiServiceApplication.java
@@ -1,9 +1,14 @@
 package com.fitness.testapiservice;
 
+import com.fitness.testapiservice.model.Exercise;
+import com.fitness.testapiservice.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -17,6 +22,25 @@ public class TestApiServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApiServiceApplication.class, args);
+    }
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init(){
+        return args -> {
+
+            Exercise first = Exercise.builder()
+                    .description("plank")
+                    .lengthInSeconds(100)
+                    .gifUrl("https://media1.giphy.com/media/2UqZvQq4p9DjTBDVRc/giphy.gif")
+                    .build();
+
+            exerciseRepository.save(first);
+
+        };
     }
 
     @Bean
