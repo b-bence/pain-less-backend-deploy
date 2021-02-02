1ae84e90bfd5a86bb095022984956fc7e7501f8d
Bence <borsi92@gmail.com>
2021-02-02 21:10:47 +0100

    created ExerciseList class

--- test-api-service/src/main/java/com/fitness/testapiservice/service/ExerciseList.java
+++ test-api-service/src/main/java/com/fitness/testapiservice/service/ExerciseList.java
@@ -1,2 +1,13 @@
-package com.fitness.testapiservice.service;public class ExerciseList {
+package com.fitness.testapiservice.service;
+
+import com.fitness.testapiservice.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExerciseList {
+
+
+    @Autowired
+    private ExerciseRepository exerciseRepository;
 }
