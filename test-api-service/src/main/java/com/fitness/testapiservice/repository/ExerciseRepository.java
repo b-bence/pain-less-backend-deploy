4b60ee940cd6489c395657b1dfc40a0af7ae7918
Bence <borsi92@gmail.com>
2021-02-02 21:10:22 +0100

    created ExerciseRepository

--- test-api-service/src/main/java/com/fitness/testapiservice/repository/ExerciseRepository.java
+++ test-api-service/src/main/java/com/fitness/testapiservice/repository/ExerciseRepository.java
@@ -1,2 +1,11 @@
-package com.fitness.testapiservice.model;public interface ExerciseRepository {
+package com.fitness.testapiservice.repository;
+
+import com.fitness.testapiservice.model.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+    List<Exercise> getExercisesByLengthInSecondsGreaterThan(int length);
 }
