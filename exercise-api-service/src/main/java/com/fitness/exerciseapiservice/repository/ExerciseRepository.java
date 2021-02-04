71cb62e309b9da6bf8ccd46b2f91395262af51ab
Bence <borsi92@gmail.com>
2021-02-04 14:42:11 +0100

    added getExerciseById query

--- exercise-api-service/src/main/java/com/fitness/exerciseapiservice/repository/ExerciseRepository.java
+++ exercise-api-service/src/main/java/com/fitness/exerciseapiservice/repository/ExerciseRepository.java
@@ -3,9 +3,9 @@ package com.fitness.exerciseapiservice.repository;
 import com.fitness.exerciseapiservice.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.UUID;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
-    List<Exercise> getExercisesByLengthInSecondsGreaterThan(int length);
+    Exercise getExerciseById(UUID id);
 }
