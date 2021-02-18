d958ecf61eab2b5cdccbdf695643e05f195d02ba
Bence <borsi92@gmail.com>
2021-02-18 16:24:52 +0100

    added query to get exercise by id

--- src/main/java/com/fitness/exerciseapiservice/repository/WorkoutRepository.java
+++ src/main/java/com/fitness/exerciseapiservice/repository/WorkoutRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
 
-
+    Workout getWorkoutById(Long id);
 }
