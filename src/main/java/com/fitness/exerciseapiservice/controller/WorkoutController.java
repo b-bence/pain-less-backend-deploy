c9bc559dadcf6940a9dfd6a1472f7257f4694367
Bence <borsi92@gmail.com>
2021-02-18 16:25:09 +0100

    added endpoint to get exercise by id

--- src/main/java/com/fitness/exerciseapiservice/controller/WorkoutController.java
+++ src/main/java/com/fitness/exerciseapiservice/controller/WorkoutController.java
@@ -5,6 +5,7 @@ import com.fitness.exerciseapiservice.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +22,11 @@ public class WorkoutController {
     @GetMapping("/workout/all")
     public List<Workout> getAllWorkouts(){
         return workoutRepository.findAll();
+    }
+
+    @CrossOrigin(origins = ORIGIN)
+    @GetMapping("/workout/{id}")
+    public Workout getExerciseById(@PathVariable Long id){
+        return workoutRepository.getWorkoutById(id);
     }
 }
