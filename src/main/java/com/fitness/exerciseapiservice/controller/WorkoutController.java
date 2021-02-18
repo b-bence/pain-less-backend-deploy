adf4f4a1e31ef3f01d1e75361ee260671f821202
Bence <borsi92@gmail.com>
2021-02-18 13:54:10 +0100

    added workout controller

---
+++ src/main/java/com/fitness/exerciseapiservice/controller/WorkoutController.java
@@ -0,0 +1,25 @@
+package com.fitness.exerciseapiservice.controller;
+
+import com.fitness.exerciseapiservice.model.Workout;
+import com.fitness.exerciseapiservice.repository.WorkoutRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class WorkoutController {
+
+    private static final String ORIGIN = "*";
+
+    @Autowired
+    private WorkoutRepository workoutRepository;
+
+    @CrossOrigin(origins = ORIGIN)
+    @GetMapping("/workout/all")
+    public List<Workout> getAllWorkouts(){
+        return workoutRepository.findAll();
+    }
+}
